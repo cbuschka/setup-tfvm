9df50fcbaeb1ecaff161b452bc6b359f4e7a6135
conni <cbuschka@gmail.com>
2022-12-15 14:34:08 +0100

    #11 Added version number format check.

--- src/latest-tfvm-version.js
+++ src/latest-tfvm-version.js
@@ -2,22 +2,25 @@ const fetch = require('node-fetch');
 
 const getLatestTfvmVersion = async () => {
   const LATEST_RELEASE_URL = "https://api.github.com/repos/cbuschka/tfvm/releases/latest";
-  let response = null;
   try {
-    response = await fetch(LATEST_RELEASE_URL, {
+    const response = await fetch(LATEST_RELEASE_URL, {
       method: 'GET',
       headers: {'Content-Type': 'application/json'},
     });
+    const body = await response.json();
+    const version = body["tag_name"];
+    if (!version) {
+      throw new Error("no tag_name");
+    }
+
+    if ( !/^v\d+\.\d+(\.\d+)?$/.test(version) ) {
+      throw new Error("'"+version+"' is not a valid version number");
+    }
+
+    return version;
   } catch( err ) {
     throw new Error("Getting latest tfvm release failed ("+err.message+").");
   }
- 
-  const body = await response.json();
-  const version = body["tag_name"];
-  if (!version) {
-    throw new Error("Getting latest tfvm release failed (no tag_name).");
-  }
-  return version;
 };
 
 module.exports = getLatestTfvmVersion;
