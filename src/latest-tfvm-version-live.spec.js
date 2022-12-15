6ea16dfa4eca56cbe7c2d6b2a1578397a3c5b65c
conni <cbuschka@gmail.com>
2022-12-15 14:08:59 +0100

    #11 Renamed latest-tfvm-version.spec.js to latest-tfvm-version-live.spec.js.

--- src/latest-tfvm-version-live.spec.js
+++ src/latest-tfvm-version-live.spec.js
@@ -1,10 +1,8 @@
 const getLatestTfvmVersion = require('./latest-tfvm-version');
 
-describe("tfvm latest version support", () => {
+describe("latest tfvm version from github", () => {
   test('happy path', async () => {
     const version = await getLatestTfvmVersion();
     expect(version).toMatch(/^v\d+\.\d+(\.\d+)?$/);
   });
-
-  
 });
