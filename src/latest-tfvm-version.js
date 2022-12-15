5462fea4b59f7d95e71b498f01e375ced373d357
conni <cbuschka@gmail.com>
2022-12-15 14:28:17 +0100

    #11 Added unit tests to latest version error handling.

--- src/latest-tfvm-version.js
+++ src/latest-tfvm-version.js
@@ -2,14 +2,20 @@ const fetch = require('node-fetch');
 
 const getLatestTfvmVersion = async () => {
   const LATEST_RELEASE_URL = "https://api.github.com/repos/cbuschka/tfvm/releases/latest";
-  const response = await fetch(LATEST_RELEASE_URL, {
-    method: 'GET',
-    headers: {'Content-Type': 'application/json'},
-  });
+  let response = null;
+  try {
+    response = await fetch(LATEST_RELEASE_URL, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+    });
+  } catch( err ) {
+    throw new Error("Getting latest tfvm release failed ("+err.message+").");
+  }
+ 
   const body = await response.json();
   const version = body["tag_name"];
   if (!version) {
-    throw new Error("Getting latest tfvm release failed.");
+    throw new Error("Getting latest tfvm release failed (no tag_name).");
   }
   return version;
 };
